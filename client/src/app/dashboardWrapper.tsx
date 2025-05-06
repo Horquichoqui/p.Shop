f7fe97e5e82f83212639d379f6fa452409da7448
Gabriel Job <gabrielpjob2002@gmail.com>
2025-05-06 18:56:32 -0300

    feat: redux persist and ajusts

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,17 +1,34 @@
+"use client";
 import React from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "./redux";
 
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSideBarCollapsed
+  );
+  return (
+    <StoreProvider>
+      <div className={`dark flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+        <Sidebar />
+        <main
+          className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
+            isSideBarCollapsed ? "md:pl-24" : "md:pl-72"
+          }`}
+        >
+          <Navbar />
+          {children}
+        </main>
+      </div>
+    </StoreProvider>
+  );
+};
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={`dark flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      
-      <Sidebar/>
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
-        <Navbar/>
-        {children}
-      </main>
-    </div>
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
