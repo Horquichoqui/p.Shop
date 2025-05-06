9349a13954895abf9e05110a142a1622a1ffb88d
Gabriel Job <gabrielpjob2002@gmail.com>
2025-05-06 19:13:26 -0300

    Feat: Add dark and light mode

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "@/app/(components)/Navbar";
 import Sidebar from "@/app/(components)/Sidebar";
 import StoreProvider, { useAppSelector } from "./redux";
@@ -8,9 +8,21 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isSideBarCollapsed = useAppSelector(
     (state) => state.global.isSideBarCollapsed
   );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  });
   return (
     <StoreProvider>
-      <div className={`dark flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      <div
+        className={`${
+          isDarkMode ? "dark" : "light"
+        } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+      >
         <Sidebar />
         <main
           className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${
